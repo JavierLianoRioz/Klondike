adc9d9781d54a537bb6535ebb9d027cda611365a
midas@tower.windows <javier.liano@alumnos.uneatlantico.es>
2025-06-11 19:42:38 +0200

    feat: agregar clases Cliente

---
+++ src/Klondike.java
@@ -0,0 +1,9 @@
+
+public class Klondike {
+
+    public void iniciar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'iniciar'");
+    }
+
+}
